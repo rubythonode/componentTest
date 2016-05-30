acdd599d6f2c9d6a1ce5b6cdff59d2810aa9cd55
Cheol <g6lingp@gmail.com>
2016-05-31 03:13:50 +0900

    add tabBar Icon

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
+
+
 # 사용
 
 1. `git clone https://github.com/g6ling/componentTest.git`
 2. `npm install`
-3. `react-native run-ios`
-4. `git checkout -b 형이원하는이름`
+3. `rnpm link`  https://github.com/rnpm/rnpm 들어가서 rnpm 설치후 link실행
+4. `react-native run-ios`
+5. `git checkout -b 형이원하는이름`
 
 
 # Todo
 - [x] ~~소셜 로그인 추가~~
 - [ ] 수업 클릭 후 수업 상세 페이지
 - [ ] 스케쥴(달력) 페이지
-- [ ] 탭바 아이콘 + 색깔 변경
+- [x] ~~탭바 아이콘 + 색깔 변경~~
 - [x] ~~수업 Setting에 알맞게 숫자조정~~
 
 여기에 혹시 해야 할게 있다면 추가.
--- navigator.js
+++ navigator.js
@@ -16,12 +16,14 @@ import UserProfileSetting from './component/userProfileSetting'
 import GetSchool from './component/getSchool'
 
 import TimeTable from './component/timeTable'
+import ScheduleCalendar from './component/scheduleCalendar'
 
 import TimeTableSetting from './component/timeTableSetting'
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
 
-const icon = <Ionicons name="ios-settings" color="gray" size={25}/>
+const SettingIcon = <Ionicons name="ios-settings" color="gray" size={25}/>
 
 
 export default class App extends Component {
@@ -35,9 +37,12 @@ export default class App extends Component {
           <Scene key="userProfileSetting" component={UserProfileSetting} title="UserProfileSetting"/>
           <Scene key="getSchool" component={GetSchool} title="GetSchool"/>
           <Scene key="tabbar" tabs={true} component={TabBar} tabBarStyle={{backgroundColor: '#FFA000'}} hideNavBar={true}>
-            <Scene key="timeTableTab" component={TimeTable} title="TimeTable"
-                  initial={true} icon={TabIcon} rightTitle={icon} onRight={()=>{alert('setting')}}
-                  ></Scene>
+                <Scene key="timeTableTab" component={TimeTable} title="TimeTable"
+                  initial={true} icon={TabIcon} rightTitle={SettingIcon} onRight={()=>{alert('setting')}}
+                  />
+                <Scene key="scheduleCalendar" component={ScheduleCalendar} title="ScheduleCalendar"
+                  icon={TabIcon}
+                  />
           </Scene>
           <Scene key="timeTableSetting" component={TimeTableSetting} title="TimeTableSetting" />
         </Scene>
@@ -46,10 +51,43 @@ export default class App extends Component {
   }
 }
 
+
+
 class TabIcon extends Component {
     render(){
-        return (
-            <Text style={{color: this.props.selected ? 'white' :'gray'}}>{this.props.title}</Text>
-        );
+      let title = this.props.title
+      let selected = this.props.selected
+      switch (title) {
+        case "TimeTable":
+          return <TimeTableIcon selected={selected} />
+          break;
+        case "ScheduleCalendar":
+          return <ScheduleCalendarIcon selected={selected} />
+          break;
+        }
     }
+}
+
+class TimeTableIcon extends Component {
+  render() {
+    let color = this.props.selected ? '#FFFFFF' : '#A0A0A0'
+    return (
+      <View style={{flexDirection: 'column', alignItems: 'center', height: 32,}}>
+        <Entypo name="sweden" color={color} size={28} />
+        <Text style={{color: color, fontSize: 10}} >時間割</Text>
+      </View>
+    )
+  }
+}
+
+class ScheduleCalendarIcon extends Component {
+  render() {
+    let color = this.props.selected ? '#FFFFFF' : '#A0A0A0'
+    return(
+      <View style={{flexDirection: 'column', alignItems: 'center', height: 32,}}>
+        <Ionicons name="ios-calendar" color={color} size={28} />
+        <Text style={{color: color, fontSize: 10}} >カレンダー</Text>
+      </View>
+    )
+  }
 }

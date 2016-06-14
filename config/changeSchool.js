6b13dc4fcbcd5d6530b019bcb174ba56eda96a97
Cheol <g6lingp@gmail.com>
2016-06-15 00:13:32 +0900

    add changeSchool// This function change Japan SchoolName to English SchoolName

---
+++ config/changeSchool.js
@@ -0,0 +1,11 @@
+var changeSchool = function(jpSchoolName) {
+  switch (jpSchoolName) {
+    case '大阪大学':
+      return 'osakaUniv'
+      break;
+    case '京都大学':
+      return 'kyotoUniv'
+      break;
+  }
+}
+export default changeSchool
